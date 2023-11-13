08fa1ab3cb94f4c83f5eb9b062c1c307f575b5bf
Jeremy Richter <jeremy.richter@mention.de>
2023-11-13 13:29:41 +0100

    Added Readme

---
+++ W_Project/README.cs
@@ -0,0 +1,9 @@
+﻿// 1. Es wird so Entwickelt, wie der Prototyp es vorsieht
+
+// 2. Es wird nur Committet, wenn man builden kann
+
+// 3. Wiederverwendbare Methoden (sollten sie existieren), wird eine Beschreibung mit "Summary" hinzugefügt
+
+// 4. Möglichst nur wenn nötig Events benutzen, bitte MVVM benutzen. Model -> Viewmodel -> View
+
+// 5. Clean Code scheiß egal, solange man es gut lesen kann
